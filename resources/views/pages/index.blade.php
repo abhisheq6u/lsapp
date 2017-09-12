5efe2c54b6a10450bbb4bf989f1fb3c26dd072db
Abhishek Kumawat <abhishek96.ak@gmail.com>
2017-09-13 00:42:25 +0530

    added nav, 4th tut complete

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-        <h1>{{ $title }}</h1>
-        <p>This is my first laravel application.</p>
+        <div class="jumbotron text-center">
+                <h1>{{ $title }}</h1>
+                <p>This is my first laravel application.</p>
+                <a class="btn btn-primary btn-lg" href="#">Login</a>
+                <a class="btn btn-success btn-lg" href="#">Register</a>
+        </div>
 @endsection
